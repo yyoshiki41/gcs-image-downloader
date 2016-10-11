ae332210455b19f315fab10d92210dfae4904b7f
yyoshiki41 <yyoshiki41@gmail.com>
2016-10-11 23:17:03 +0900

    Add outputs option

--- downloader.go
+++ downloader.go
@@ -11,14 +11,16 @@ import (
 )
 
 var (
-	confPath string
-	q        string
+	confPath    string
+	outputsPath string
+	q           string
 )
 
 func Run(args []string) {
 	app := kingpin.New("downloader", "Image downloader for Google Custom Search API.")
 
 	app.Flag("conf", "config file path").Default("conf").Short('c').StringVar(&confPath)
+	app.Flag("outputs", "Directory for downloaded files").Default("outputs").Short('o').StringVar(&outputsPath)
 	app.Flag("query", "query").Required().Short('q').StringVar(&q)
 
 	kingpin.MustParse(app.Parse(args))
